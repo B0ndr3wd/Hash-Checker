f6a6610c7fabcf80f19d468bd22be869a5abb85c
B0ndr3wd <timeo.dubreu@gmail.com>
2026-02-08 12:46:44 +0100

    Its finish

--- Check_MD5.py
+++ Check_MD5.py
@@ -1,3 +1,12 @@
-def check_MD5():
+import re
+
+def check_MD5(hash):
     '''Check if ur hash is MD5'''
-    pass
+    x = re.findall(r'[a-fA-F0-9]{32}', hash)
+    if x:
+        return True
+    
+
+assert check_MD5("5d41402abc4b2a76b9719d911017c592") == True
+assert not check_MD5("5d41402abc4b2a76b9719d911017c")
+assert not check_MD5("5d41402abc4b2a76b9719d911017c***")
