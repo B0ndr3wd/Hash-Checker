de39c8bf9e01f4bf2ae24c4b251fba9cc0141b6d
B0ndr3wd <timeo.dubreu@gmail.com>
2026-02-08 12:56:42 +0100

    Added #

--- Check_MD5.py
+++ Check_MD5.py
@@ -6,7 +6,7 @@ def check_MD5(hash):
     if x:
         return True
     
-
+# Test Function
 assert check_MD5("5d41402abc4b2a76b9719d911017c592") == True
 assert not check_MD5("5d41402abc4b2a76b9719d911017c")
 assert not check_MD5("5d41402abc4b2a76b9719d911017c***")
