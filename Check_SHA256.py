8d9c3bb365f6d8192c5367100e42273f8d2cfe79
B0ndr3wd <timeo.dubreu@gmail.com>
2026-02-08 13:00:52 +0100

    Its finish

--- Check_SHA256.py
+++ Check_SHA256.py
@@ -1,3 +1,11 @@
-def check_SHA256():
+import re
+
+def check_SHA256(hash):
     '''Check if ur hash is SHA256'''
-    pass
+    x = re.findall(r'[a-fA-F0-9]{64}', hash)
+    if x :
+        return True
+    
+# Test function
+assert check_SHA256("2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824") == True
+assert not check_SHA256("2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e730433629")
