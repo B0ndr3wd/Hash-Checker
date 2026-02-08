550bcc59689f3d5481c132d6bc2c47fe2976cf2b
B0ndr3wd <timeo.dubreu@gmail.com>
2026-02-08 12:50:04 +0100

    Adjust parameters

--- Hash_Check.py
+++ Hash_Check.py
@@ -2,13 +2,13 @@ from Check_MD5 import *
 from Check_SHA1 import *
 from Check_SHA256 import *
 
-def hash_check():
+def hash_check(hash):
     '''MD5 / SHA1 / SHA256'''
-    if check_MD5():
+    if check_MD5(hash):
         print("Its MD5 !")
-    elif check_SHA1():
+    elif check_SHA1(hash):
         print("Its SHA1 !")
-    elif check_SHA256():
+    elif check_SHA256(hash):
         print("Its SHA256 !")
     else:
         print("Its not MD5 or SHA1 or SHA256 :( ")
