f75d8a02aeda1d32369c1dce31b78c18f2b6c707
B0ndr3wd <timeo.dubreu@gmail.com>
2026-02-08 12:57:38 +0100

    Its finish

--- Check_SHA1.py
+++ Check_SHA1.py
@@ -1,3 +1,10 @@
-def check_SHA1():
+import re
+def check_SHA1(hash):
     '''Check if ur hash is SHA1'''
-    pass
+    x = re.findall(r'[a-fA-F0-9]{40}', hash)
+    if x:
+        return True
+    
+# Test the function
+assert check_SHA1("aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d") == True
+assert not check_SHA1("5d41402abc4b2a76b9719d911017c592")
